070e93b16f397dd0072afe566b41b9aa649a9077
antonymarcano <antony@antonymarcano.com>
2015-01-03 14:29:14 +0000

    Using old -not recommended but less copy-paste programming required- approach to customising the appearance of a theme

--- fullby-premium-customised/functions.php
+++
@@ -1,7 +0,0 @@
-<?php // CUSTOM STYLES
-function dkmr_enqueue_styles() {
-  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
-}
-add_action( 'wp_enqueue_scripts', 'dkmr_enqueue_styles' );
-?>
